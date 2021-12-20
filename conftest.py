5b46488c85f36dd2b17cc0c90f5fdc90e596b418
lonar <zceimlo@ucl.ac.uk>
2021-12-20 22:14:35 +0000

    GitHub Workflows

--- conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from user import GeneralUser
-
-
-@pytest.fixture(scope='function')
-def general_user():
-    new_user = GeneralUser(first_name='James', last_name='White', email='email@email.com', password="my_password",
-                           dob="30/04/98")
-    yield new_user
